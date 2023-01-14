a60178e22b54872641737932c71d41ff8a7fae3a
Pavel Temnikov <pashatemnik@gmail.com>
2023-01-14 18:14:50 +0400

    array-algorithms.service.ts - create containsDuplicate() algorithm

--- src/app/algorithms/services/array-algorithms/array-algorithms.service.ts
+++ src/app/algorithms/services/array-algorithms/array-algorithms.service.ts
@@ -7,4 +7,19 @@ import { AlgorithmsModule } from '../../algorithms.module';
 export class ArrayAlgorithmsService {
 
     constructor() { }
+
+    // https://leetcode.com/problems/contains-duplicate/
+    // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+    // O(n) - time
+    // O(n) - space
+    containsDuplicate(nums: number[]): boolean {
+        const cache: {[key: number]: boolean} = {};
+        for (const n of nums) {
+            if (cache[n]) {
+                return true;
+            }
+            cache[n] = true;
+        }
+        return false;
+    }
 }
