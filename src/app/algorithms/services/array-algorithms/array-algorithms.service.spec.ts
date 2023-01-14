1614e77061d7dce784f536ec6fe9e58620ebfbb3
Pavel Temnikov <pashatemnik@gmail.com>
2023-01-14 17:59:57 +0400

    array-algorithms.service.spec add test for containsDuplicate()

--- src/app/algorithms/services/array-algorithms/array-algorithms.service.spec.ts
+++ src/app/algorithms/services/array-algorithms/array-algorithms.service.spec.ts
@@ -3,14 +3,26 @@ import { TestBed } from '@angular/core/testing';
 import { ArrayAlgorithmsService } from './array-algorithms.service';
 
 describe('ArrayAlgorithmsService', () => {
-  let service: ArrayAlgorithmsService;
+    let service: ArrayAlgorithmsService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ArrayAlgorithmsService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ArrayAlgorithmsService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('containsDuplicate()', () => {
+        let nums = [1,2,3,1];
+        expect(service.containsDuplicate(nums)).toBeTrue();
+
+        nums = [1,2,3,4];
+        expect(service.containsDuplicate(nums)).toBeFalse();
+
+        nums = [1,1,1,3,3,4,3,2,4,2];
+        expect(service.containsDuplicate(nums)).toBeTrue();
+        
+    });
 });
