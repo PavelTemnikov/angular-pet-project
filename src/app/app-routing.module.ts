85387ce2dbb559a5e145a58ff11115ea7697d056
Pavel Temnikov <pashatemnik@gmail.com>
2023-01-18 16:11:40 +0400

    testing.module - create boilerplate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     { path: 'rxjs', loadChildren: () => import('./rxjs/rxjs.module').then(m => m.RxjsModule) }, 
-    { path: 'algorithms', loadChildren: () => import('./algorithms/algorithms.module').then(m => m.AlgorithmsModule) 
-}];
+    { path: 'algorithms', loadChildren: () => import('./algorithms/algorithms.module').then(m => m.AlgorithmsModule) },
+    { path: 'testing', loadChildren: () => import('./testing/testing.module').then(m => m.TestingModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
