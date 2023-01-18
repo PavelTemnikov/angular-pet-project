286587b37e8e1495077da651e2865a0ab9fce303
Pavel Temnikov <pashatemnik@gmail.com>
2023-01-18 16:08:24 +0400

    sorting-algorithms.service.spec - comment

--- src/app/algorithms/services/sorting-algorithms/sorting-algorithms.service.spec.ts
+++ src/app/algorithms/services/sorting-algorithms/sorting-algorithms.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
-import { SortingAlgorithmsService } from './sorting-algorithms.service';
+// import { SortingAlgorithmsService } from './sorting-algorithms.service';
 
-describe('SortingAlgorithmsService', () => {
-  let service: SortingAlgorithmsService;
+// describe('SortingAlgorithmsService', () => {
+//   let service: SortingAlgorithmsService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SortingAlgorithmsService);
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(SortingAlgorithmsService);
+//   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
