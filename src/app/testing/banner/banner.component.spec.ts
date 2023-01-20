25707e4dc9f20ac8528a3be0961a1750b199b8cf
Pavel Temnikov <pashatemnik@gmail.com>
2023-01-20 14:04:48 +0400

    banner.component.spec - add detectChanges()

--- src/app/testing/banner/banner.component.spec.ts
+++ src/app/testing/banner/banner.component.spec.ts
@@ -3,20 +3,30 @@ import { ComponentFixture, waitForAsync, TestBed } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 
 describe('BannerComponent', () => {
-    let comp: BannerComponent;
     let fixture: ComponentFixture<BannerComponent>;
+    let comp: BannerComponent;
+    let h1: HTMLHeadingElement | null;
 
-    beforeEach(waitForAsync(() => {
-        TestBed.configureTestingModule({
-            declarations: [BannerComponent]
-        })
-        .compileComponents();
-    }));
+    // beforeEach(waitForAsync(() => {
+    //     TestBed.configureTestingModule({
+    //         declarations: [BannerComponent]
+    //     })
+    //     .compileComponents();
+    // }));
+
+    // beforeEach(() => {
+    //     fixture = TestBed.createComponent(BannerComponent);
+    //     comp = fixture.componentInstance;
+    //     fixture.detectChanges();
+    // });
 
     beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [BannerComponent]
+        });
         fixture = TestBed.createComponent(BannerComponent);
         comp = fixture.componentInstance;
-        fixture.detectChanges();
+        h1 = (fixture.nativeElement as HTMLElement).querySelector('h1') ;
     });
 
     it('should create', () => {
@@ -41,5 +51,20 @@ describe('BannerComponent', () => {
             .nativeElement as HTMLElement;
 
         expect(p.textContent).toBe('Hello, I am banner');
+    });
+
+    it('no title in the DOM after createComponent()', () => {
+        expect(h1?.textContent).toBe('');
+    });
+
+    it('should display original title after detectChanges()', () => {
+        fixture.detectChanges();
+        expect(h1?.textContent).toBe('My Super Title');
+    });
+
+    it('should display a different test title', () => {
+        comp.title = 'Changed Title';
+        fixture.detectChanges();
+        expect(h1?.textContent).toBe('Changed Title');
     });
 });
--- src/app/testing/banner/banner.component.ts
+++ src/app/testing/banner/banner.component.ts
@@ -2,8 +2,12 @@ import { Component} from "@angular/core";
 
 @Component({
     selector: 'banner',
-    template: `<p>Hello, I am banner</p>`
+    template: `
+        <h1>{{title}}</h1>
+        <p>Hello, I am banner</p>
+    `,
+    styles: ['h1 { color: green; font-szie: 350% }']
 })
 export class BannerComponent {
-
+    title = 'My Super Title';
 }
