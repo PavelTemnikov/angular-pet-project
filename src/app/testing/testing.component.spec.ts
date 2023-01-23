2f01f835b14379fc57bb3eef96c2d6949edb3617
Pavel Temnikov <pashatemnik@gmail.com>
2023-01-23 14:17:23 +0400

    testing.component.spec - fix code style

--- src/app/testing/testing.component.spec.ts
+++ src/app/testing/testing.component.spec.ts
@@ -1,23 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TestingComponent } from './testing.component';
+import { TwainComponent } from './twain/twain.component';
+import { TwainService } from './twain/twain.service';
 
 describe('TestingComponent', () => {
-  let component: TestingComponent;
-  let fixture: ComponentFixture<TestingComponent>;
+    let component: TestingComponent;
+    let fixture: ComponentFixture<TestingComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TestingComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [HttpClientModule],
+            declarations: [TestingComponent, TwainComponent],
+            providers: [TwainService]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(TestingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(TestingComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
