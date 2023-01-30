8504a0f1ec0673adaf478dd8c4f96af2ed3e6e26
PavelTemnikov <pashatemnik@gmail.com>
2023-01-30 17:14:03 +0400

    search-algorithms.service - add binarySearch and tests

---
+++ src/app/algorithms/services/search-algorithms/search-algorithms.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { AlgorithmsModule } from '../../algorithms.module';
+
+@Injectable({
+  providedIn: AlgorithmsModule
+})
+export class SearchAlgorithmsService {
+
+  binarySearch(sortedArray: number[], seekElement: number): number {
+    let startIndex = 0;
+    let endIndex = sortedArray.length - 1;
+    let middleIndex: number;
+
+    while (endIndex >= startIndex) {
+      middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+      if (seekElement === sortedArray[middleIndex]) {
+        return middleIndex;
+      }
+      else if (seekElement > sortedArray[middleIndex]) {
+        startIndex = middleIndex + 1;
+      }
+      else {
+        // seekElement < sortedArray[middleIndex]
+        endIndex = middleIndex - 1;
+      }
+    }
+    return -1;
+  }
+
+}
