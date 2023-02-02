3553e48a1f7ee3787fe10fa41b6e30cfa0a364fc
Pavel Temnikov <pashatemnik@gmail.com>
2023-02-02 22:17:14 +0400

    heap.ts - rearrange methods

--- src/app/algorithms/data-structures/heap.ts
+++ src/app/algorithms/data-structures/heap.ts
@@ -3,17 +3,17 @@ export class MinNumberHeap {
     private length = 0;
     private items: (number | undefined)[] = new Array(10);
 
-    public leftChildIndex (index: number): number { return 2 * index + 1; }
-    public rightChildIndex(index: number): number { return 2 * index + 2; }
-    public parentIndex    (index: number): number { return Math.floor( (index - 1) / 2 ); }
+    private leftChildIndex (index: number): number { return 2 * index + 1; }
+    private rightChildIndex(index: number): number { return 2 * index + 2; }
+    private parentIndex    (index: number): number { return Math.floor( (index - 1) / 2 ); }
 
-    public hasLeftChild (index: number): boolean { return this.leftChildIndex(index) < this.length; }
-    public hasRightChild(index: number): boolean { return this.rightChildIndex(index) < this.length; }
-    public hasParent    (index: number): boolean { return this.parentIndex(index) >= 0; }
+    private hasLeftChild (index: number): boolean { return this.leftChildIndex(index) < this.length; }
+    private hasRightChild(index: number): boolean { return this.rightChildIndex(index) < this.length; }
+    private hasParent    (index: number): boolean { return this.parentIndex(index) >= 0; }
 
-    public leftChild (index: number): number | undefined { return this.items[ this.leftChildIndex(index) ]; }
-    public rightChild(index: number): number | undefined { return this.items[ this.rightChildIndex(index) ]; }
-    public parent    (index: number): number | undefined { return this.items[ this.parentIndex(index) ]; }
+    private leftChild (index: number): number | undefined { return this.items[ this.leftChildIndex(index) ]; }
+    private rightChild(index: number): number | undefined { return this.items[ this.rightChildIndex(index) ]; }
+    private parent    (index: number): number | undefined { return this.items[ this.parentIndex(index) ]; }
 
     private swap(indexOne: number, indexTwo: number): void {
         [ this.items[indexOne], this.items[indexTwo] ] = [ this.items[indexTwo], this.items[indexOne] ];
@@ -24,7 +24,33 @@ export class MinNumberHeap {
             const tempItems = this.items;
             this.capacity *= 2;
             this.items = new Array(this.capacity);
-            tempItems.forEach((item, i) => this.items[i] = item);
+            for (let i = 0; i< tempItems.length; i++) { this.items[i] = tempItems[i]; }
+        }
+    }
+
+    private heapifyUp(): void {
+        if (this.length === 0) {
+            return;
+        }
+        let index = this.length - 1;
+        while (this.hasParent(index) && (this.parent(index) as number) > (this.items[index] as number)) {
+            this.swap(this.parentIndex(index), index);
+            index = this.parentIndex(index);
+        }
+    }
+
+    private heapifyDown(): void {
+        let index = 0;
+        while (this.hasLeftChild(index)) {
+            let smallerChaildIndex = this.leftChildIndex(index);
+            if (this.hasRightChild(index) && (this.rightChild(index) as number) < (this.leftChild(index) as number)) {
+                smallerChaildIndex = this.rightChildIndex(index);
+            }
+            if ((this.items[index] as number) < (this.items[smallerChaildIndex] as number)) {
+                break;
+            }
+            this.swap(smallerChaildIndex, index);
+            index = smallerChaildIndex;
         }
     }
 
@@ -53,29 +79,5 @@ export class MinNumberHeap {
         this.heapifyUp();
     }
 
-    public heapifyUp(): void {
-        if (this.length === 0) {
-            return;
-        }
-        let index = this.length - 1;
-        while (this.hasParent(index) && (this.parent(index) as number) > (this.items[index] as number)) {
-            this.swap(this.parentIndex(index), index);
-            index = this.parentIndex(index);
-        }
-    }
-
-    public heapifyDown(): void {
-        let index = 0;
-        while (this.hasLeftChild(index)) {
-            let smallerChaildIndex = this.leftChildIndex(index);
-            if (this.hasRightChild(index) && (this.rightChild(index) as number) < (this.leftChild(index) as number)) {
-                smallerChaildIndex = this.rightChildIndex(index);
-            }
-            if ((this.items[index] as number) < (this.items[smallerChaildIndex] as number)) {
-                break;
-            }
-            this.swap(smallerChaildIndex, index);
-            index = smallerChaildIndex;
-        }
-    }
+    public isEmpty(): boolean { return this.length === 0; }
 }
