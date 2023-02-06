b9dc45f3d5eaf4ee787e82a7f0c4820f5c6b2971
PavelTemnikov <pashatemnik@gmail.com>
2023-02-06 17:57:50 +0400

    ngxs

--- src/app/ngxs/states/zoo.state.ts
+++ src/app/ngxs/states/zoo.state.ts
@@ -1,15 +1,17 @@
 import { Injectable } from "@angular/core";
 import { Action, State, StateContext } from "@ngxs/store";
-import { FeedAnimals } from "../actions/animal.actions";
+import { AddAnimal, FeedAnimals } from "../actions/animal.actions";
 
 export interface ZooStateModel {
     feed: boolean;
+    animals: string[];
 }
 
 @State<ZooStateModel>({
     name: 'zoo',
     defaults: {
-        feed: false
+        feed: false,
+        animals: []
     }
 })
 @Injectable()
@@ -17,9 +19,20 @@ export class ZooState {
     @Action(FeedAnimals)
     feedAnimals(ctx: StateContext<ZooStateModel>) {
         const state = ctx.getState();
-        ctx.setState({
-            ...state,
-            feed: !state.feed
-        });
+        // ctx.setState({
+        //     ...state,
+        //     feed: !state.feed
+        // });
+        ctx.patchState({ feed: !state.feed })
+    }
+
+    @Action(AddAnimal)
+    addAnimal(ctx: StateContext<ZooStateModel>, action: AddAnimal) {
+        const state = ctx.getState();
+        // ctx.setState({
+        //     ...state,
+        //     animals: [...state.animals, action.name]
+        // });
+        ctx.patchState({ animals: [...state.animals, action.name] });
     }
 }
--- src/app/ngxs/zoo/zoo.component.ts
+++ src/app/ngxs/zoo/zoo.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Select, Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { Observable, withLatestFrom } from 'rxjs';
 import { AddAnimal, FeedAnimals } from '../actions/animal.actions';
 import { ZooStateModel } from '../states/zoo.state';
 
@@ -14,6 +14,16 @@ import { ZooStateModel } from '../states/zoo.state';
         <button type="button" (click)="feedAnimals()">Feed Animals</button>
 
         <div>feed: {{feed$ | async}}</div>
+        
+        <div>
+          <ul>
+            List of animals
+            <li *ngFor="let animal of animals$ | async">{{animal}}</li>
+          </ul>
+        </div>
+        
+        <button type="button" (click)="consoleStoreState()">Console Store State</button>
+        <button type="button" (click)="resetState()">Reset State</button>
     `,
     styles: [
     ]
@@ -22,14 +32,29 @@ export class ZooComponent {
     name = new FormControl('');
 
     @Select((state: { zoo: ZooStateModel}) => state.zoo.feed) feed$!: Observable<boolean>;
+    @Select((state: { zoo: ZooStateModel}) => state.zoo.animals) animals$!: Observable<string[]>;
 
     constructor(private store: Store) {}
 
     addAnimal(): void {
-        this.store.dispatch(new AddAnimal(this.name.value || '')).subscribe(() => this.name.reset());
+        this.store
+            .dispatch(new AddAnimal(this.name.value || ''))
+            .pipe(withLatestFrom(this.animals$))
+            .subscribe(([_, animals]) => {
+                console.log(animals);
+                this.name.reset();
+            });
     }
 
     feedAnimals(): void {
         this.store.dispatch(new FeedAnimals());
+    }
+
+    consoleStoreState(): void {
+        console.log(this.store.snapshot());
+    }
+
+    resetState(): void {
+        this.store.reset({ zoo: { feed: true, animals: ['first', 'second']} })
     }
 }
