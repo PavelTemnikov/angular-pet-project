fe4d6c99ae0b9f4f34265b884b1103499398cb4d
Pavel Temnikov <pashatemnik@gmail.com>
2023-02-08 19:50:45 +0400

    del algo module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: 'rxjs',       loadChildren: () => import('./rxjs/rxjs.module').then(m => m.RxjsModule) }, 
-    { path: 'algorithms', loadChildren: () => import('./algorithms/algorithms.module').then(m => m.AlgorithmsModule) },
+    { path: 'rxjs',       loadChildren: () => import('./rxjs/rxjs.module').then(m => m.RxjsModule) },
     { path: 'testing',    loadChildren: () => import('./testing/testing.module').then(m => m.TestingModule) },
     { path: 'javascript', loadChildren: () => import('./javascript/javascript.module').then(m => m.JavascriptModule) },
     { path: 'ngxs',       loadChildren: () => import('./ngxs/ngxs.module').then(m => m.MyNgxsModule) }
