768bcbb3e9037fc2db0743cd34fb5141585d43d2
PavelTemnikov <pashatemnik@gmail.com>
2023-02-10 14:52:18 +0400

    comment unused tests

--- src/app/ngxs/ngxs.component.spec.ts
+++ src/app/ngxs/ngxs.component.spec.ts
@@ -1,23 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { NgxsComponent } from './ngxs.component';
-
-describe('NgxsComponent', () => {
-  let component: NgxsComponent;
-  let fixture: ComponentFixture<NgxsComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NgxsComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(NgxsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { NgxsComponent } from './ngxs.component';
+//
+// describe('NgxsComponent', () => {
+//   let component: NgxsComponent;
+//   let fixture: ComponentFixture<NgxsComponent>;
+//
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ NgxsComponent ]
+//     })
+//     .compileComponents();
+//
+//     fixture = TestBed.createComponent(NgxsComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
